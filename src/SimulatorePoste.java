620ccbb2a82ea6b7a2a7b44e80963fbf5fced85d
Ale-snt <santonialessio2@gmail.com>
2025-12-09 10:46:59 +0100

    Secondo SimulatorePoste.java

--- src/SimulatorePoste.java
+++ src/SimulatorePoste.java
@@ -1,18 +1,47 @@
-/**
- * Classe con il main, che avvia l'appa
- * che rappresenta il flusso dei clienti di un ufficio postale
- * messi in attesa da un totem elettronico che assegna
- * un numero progressivo e stampa il ticket
- * Clienti gestiti da un solo sportello
- * @author frida
- * @version 1.0
- */
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class SimulatorePoste {
+
     public static void main(String[] args) {
+
         ListaClienti listaClienti = new ListaClienti();
+
+        LocalTime apertura = LocalTime.now();
+
         Thread arriviThread = new Thread(new GestoreArrivi(listaClienti));
-        Thread sportelloThread = new Thread(new Sportello(listaClienti));
+        Thread sportello1 = new Thread(new Sportello(listaClienti, "Marzia"));
+        Thread sportello2 = new Thread(new Sportello(listaClienti, "Cinzia"));
+
         arriviThread.start();
-        sportelloThread.start();
+        sportello1.start();
+        sportello2.start();
+
+        try {
+            // Simulazione di 30 secondi
+            Thread.sleep(30000);
+
+            arriviThread.interrupt();
+            sportello1.interrupt();
+            sportello2.interrupt();
+
+            arriviThread.join();
+            sportello1.join();
+            sportello2.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        LocalTime chiusura = LocalTime.now();
+        Duration durata = Duration.between(apertura, chiusura);
+
+        long minuti = durata.toMinutes();
+        long secondi = durata.getSeconds() % 60;
+
+        System.out.println("\n===== STATISTICHE =====");
+        System.out.println("Apertura:\t" + apertura);
+        System.out.println("Chiusura:\t" + chiusura);
+        System.out.println("Durata:\t\t" + minuti + "min " + secondi + "sec");
     }
 }
