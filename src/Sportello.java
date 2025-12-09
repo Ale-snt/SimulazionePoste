f8de402659dd94bad1be44335e32f448839642a9
Ale-snt <santonialessio2@gmail.com>
2025-12-09 10:46:04 +0100

    Secondi Sportello.java

--- src/Sportello.java
+++ src/Sportello.java
@@ -1,47 +1,34 @@
-/**
- * Classe che implementa il thread Sportello,a
- * che serve i clienti delle
- * poste che arrivano via via, ovvero è il thread
- * consumatore delle risorse condivise
- * listaNumeri, ultimoArrivo, ultimoServito
- * @author frida
- * @version 1.0
- */
 public class Sportello implements Runnable {
-    /**
-     * risorse condivise fra i due thread
-     */
+
     private ListaClienti listaClienti;
     private final int minTempoServizio = 1000;
     private final int maxTempoServizio = 4000;
-    /**
-     * constructor
-     * @param listaClienti
-     */
-    public Sportello(ListaClienti listaClienti) {
+    private String nome;
+
+    public Sportello(ListaClienti listaClienti, String nome) {
         this.listaClienti = listaClienti;
+        this.nome = nome;
     }
 
-    /**
-     * TODO: cosa fa?
-     * @see Runnable
-     */
     public void run() {
         try {
             while (!Thread.interrupted()) {
                 Integer clienteServito = listaClienti.rimuoviCliente();
-                //tempo di servizio variabile nel range [1,4] secondi
-                int tempoServizio = (int) (Math.random() * (maxTempoServizio
-                        - minTempoServizio + 1) + minTempoServizio);
+
+                int tempoServizio = (int) (
+                    Math.random() * (maxTempoServizio - minTempoServizio + 1)
+                    + minTempoServizio
+                );
+
                 Thread.sleep(tempoServizio);
-                //Thread.sleep(1000); //tempo di servizio fisso
-                System.out.println("Servito Cliente Numero \t " + clienteServito+
-                        " dallo sportello");
+
+                System.out.println("Servito Cliente Numero \t " + clienteServito +
+                        " dallo sportello " + nome);
             }
         } catch (InterruptedException e) {
-            System.out.println("Thread interrotto durante lo sleep");
+            System.out.println("Sportello interrotto");
         } finally {
-            System.out.println("Sportello Chiuso");
+            System.out.println("Sportello " + nome + " Chiuso");
         }
     }
 }
